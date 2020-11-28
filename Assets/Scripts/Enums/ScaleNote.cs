418abcdc7a6a9d515c10e310a95a531344ffc4c7
Ethan Bowen <eab3@cbord.com>
2020-11-28 11:53:35 -0600

    Music updated to play scale as spheres are hit.

---
+++ Assets/Scripts/Enums/ScaleNote.cs
@@ -0,0 +1,65 @@
+﻿public enum ScaleNote
+{
+    G,
+    GSharp,
+    A,
+    ASharp,
+    B,
+    C,
+    CSharp,
+    D,
+    DSharp,
+    E,
+    F,
+    FSharp,
+    G5
+}
+
+public static class ScaleNoteMethods
+{
+    public static ScaleNote GetNextChromatic(ScaleNote current)
+    {
+        ScaleNote next = ScaleNote.G;
+        switch (current)
+        {
+            case ScaleNote.G:
+                next = ScaleNote.GSharp;
+                break;
+            case ScaleNote.GSharp:
+                next = ScaleNote.A;
+                break;
+            case ScaleNote.A:
+                next = ScaleNote.ASharp;
+                break;
+            case ScaleNote.ASharp:
+                next = ScaleNote.B;
+                break;
+            case ScaleNote.B:
+                next = ScaleNote.C;
+                break;
+            case ScaleNote.C:
+                next = ScaleNote.CSharp;
+                break;
+            case ScaleNote.CSharp:
+                next = ScaleNote.D;
+                break;
+            case ScaleNote.D:
+                next = ScaleNote.DSharp;
+                break;
+            case ScaleNote.DSharp:
+                next = ScaleNote.E;
+                break;
+            case ScaleNote.E:
+                next = ScaleNote.F;
+                break;
+            case ScaleNote.F:
+                next = ScaleNote.FSharp;
+                break;
+            case ScaleNote.FSharp:
+                next = ScaleNote.G5;
+                break;
+        }
+
+        return next;
+    }
+}
--- Assets/Scripts/MusicCube.cs
+++ Assets/Scripts/MusicCube.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
+﻿using UnityEngine;
 
 public class MusicCube : MonoBehaviour
 {
     private AudioSource audioSource;
     private MoveToGoal moveScript;
-    public int scoreIndex; 
+    public int scoreIndex;
     public float totalPlayTime = 0.0f;
+    public ScaleNote currentScaleNote = ScaleNote.G;
+    private bool isPlaying = false;
 
     private float m_LastStartTime;
 
@@ -19,29 +16,51 @@ public class MusicCube : MonoBehaviour
         audioSource = this.GetComponent<AudioSource>();
         moveScript = gameObject.GetComponent<MoveToGoal>();
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{audioSource.clip.name} - OnTriggerEnter: {other.GetType().ToString()} - {totalPlayTime.ToString()}");
+        Debug.Log(
+            $"{audioSource.clip.name} - OnTriggerEnter: {other.GetType().ToString()} - {totalPlayTime.ToString()}");
         if (other.GetType() == typeof(SphereCollider))
         {
             moveScript.Stun();
-        } else if (other.GetType() != typeof(BoxCollider) && !audioSource.isPlaying)
+            if (isPlaying)
+            {
+                audioSource.Stop();
+                isPlaying = false;
+            }
+
+            currentScaleNote = ScaleNoteMethods.GetNextChromatic(currentScaleNote);
+            var clip =  Resources.Load($"Music/{currentScaleNote.ToString()}") as AudioClip;
+            audioSource.clip = clip;
+            if (!isPlaying)
+            {
+                audioSource.Play();
+                isPlaying = true;
+            }
+        }
+        else if (other.GetType() != typeof(BoxCollider) && !audioSource.isPlaying)
         {
             m_LastStartTime = Time.time;
-            audioSource.Play();
-            
+            if (!isPlaying)
+            {
+                audioSource.Play();
+                isPlaying = true;
+            }
+
             ScoreBehavior.AddScore(scoreIndex);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log($"{audioSource.clip.name} - OnTriggerExit: {other.GetType().ToString()} - {totalPlayTime.ToString()}");
+        Debug.Log(
+            $"{audioSource.clip.name} - OnTriggerExit: {other.GetType().ToString()} - {totalPlayTime.ToString()}");
         if (other.GetType() != typeof(BoxCollider) && other.GetType() != typeof(SphereCollider))
         {
             totalPlayTime += Time.time - m_LastStartTime;
             audioSource.Stop();
-            
+            isPlaying = false;
             ScoreBehavior.AddScore(scoreIndex);
         }
     }
